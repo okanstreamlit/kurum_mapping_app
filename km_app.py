d75e593a35d318dc90f7e271e77e6ff570c97ef5
okanstreamlit <140599536+okanstreamlit@users.noreply.github.com>
2025-04-08 19:31:59 +0300

    sasaas

--- km_app.py
+++ km_app.py
@@ -108,7 +108,7 @@ def clean_text(text):
 
         "POLIKLINIGI":'HOSPITAL',
         'MEDICALPARK':'MEDICAL PARK',
-        'TIP MERKEZI': 'HOSPITAL'
+        'TIP MERKEZI': 'HOSPITAL',
 
         # MANUEL DUZELTME
         "ACIBADEM ETILER POL": "ACIBADEM ETILER TIP MERKEZI"
